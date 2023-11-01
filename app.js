5fb73ec5c7f218c6f188a99fefc2694b166dc20b
khushal <khushalsuhagiya033@gmail.com>
2023-11-01 17:22:31 +0530

    first

---
+++ app.js
@@ -0,0 +1,37 @@
+// let a;
+// a = "hello";
+// console.log(typeof(a));     // hello
+// console.log((a));     // hello
+// function addition(n1:number, n2:number) : number
+// {
+//     return n1 + n2;
+// }
+// let result = addition(10, 20);
+// let result = addition(10, "20");
+// console.log(result);
+// let arr: string[] | number[] = [10,20,3.5,"hello", "skill", true]
+// let arr: [number,  string, boolean?] = [10,20,"hello", "skill"]
+// arr.push(true);
+// console.log(arr);
+// type obj = {
+//     name? : string | number,
+//     age?: number,
+//     email?: string
+// }
+// let person: obj = {
+//     name: 123,
+//     age: 15
+// }
+// let person:{name?:string, age: number} = {
+//     name: 'Hello',
+//     age: 15
+// }
+// person.email = "hello@test.in";
+// person.password = 
+// console.log(person);
+// enum color {
+//     red, green, blue
+// }
+// // let x = color.yellow
+// let x = color.blue
+// console.log(x);
